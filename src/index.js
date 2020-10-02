98637249b5cd9cd1faeb50f617b9ec650f02897a
Trevor Manz <trevor.j.manz@gmail.com>
2020-10-02 16:14:32 -0400

    Add comments

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import { images } from './config.json';
 const ITERS = 10;
 
 async function timeRegions({ file, sources, regions }, iter) {
+  // TODO: Shuffle sources
   for (const { url, format, tileSize, compression } of sources) {
+
     const loader = await getLoader({ url, format });
     const { height: rasterHeight, width: rasterWidth } = loader.getRasterSize({ z: 0 })
     const extent = [0, 0, rasterWidth, rasterHeight];
+
     for (const { id, yCoord, xCoord, zoom, viewports, numChannels } of regions) {
       for (const n of numChannels) {
         const loaderSelection = range(n).map(i => ({ channel: i }));
+
         for (const [height, width] of viewports) {
+
+          // Compute the tiles needed for each region for a viewport size and zoom level.
           const tileCoords = getTileCoords({
             yCoord,
             xCoord,
@@ -22,7 +28,9 @@ async function timeRegions({ file, sources, regions }, iter) {
             extent,
           });
           const p = tileCoords.map(t => timeGetTile(loader, { ...t, loaderSelection }));
-          const times = await Promise.all(p);
+          const times = await Promise.all(p); // await all requests
+
+          // Create summary of output 
           const records = times.map(([start, end], i) => {
             const { x, y, z } = tileCoords[i];
             return {
