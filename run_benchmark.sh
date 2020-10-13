4dd01e9422d3342299021e9be35eb78345755c08
ilan-gold <ilanbassgold@gmail.com>
2020-10-13 16:45:32 -0400

    Remove quotes

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -4,5 +4,5 @@ die() { set +v; echo "$*" 1>&2 ; exit 1; }
 
 # Run 10 iterations.
 for i in $(seq 0 10); do 
-   node ./benchmark-iter.js $@ "-iter" $i
+   node ./benchmark-iter.js $@ -iter $i
 done
