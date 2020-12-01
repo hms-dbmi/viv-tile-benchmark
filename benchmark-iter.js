00e3d5ffd2fadd6eb244ea3a04fec5ccdde84e0e
ilan-gold <ilanbassgold@gmail.com>
2020-12-01 15:06:23 -0500

    One Browser Session per Iteration (#7)

--- benchmark-iter.js
+++ benchmark-iter.js
@@ -6,8 +6,9 @@ const fs = require('fs');
 
 const url = `file://${__dirname}/build/index.html`;
 
-async function run(outfile, config = {}) {
-  const stream = fs.createWriteStream(outfile);
+async function run(outfile, config = {}, iter) {
+  const stream = fs.createWriteStream(outfile, { flags: iter > 0 ? "a" : undefined });
+  const iterUrl = `${url}?iter=${iter}`;
 
   const browser = await puppeteer.launch(config);
   const page = await browser.newPage();
@@ -15,7 +16,7 @@ async function run(outfile, config = {}) {
   await page.setCacheEnabled(false);
   await page.reload({ waitUntil: 'networkidle2' });
 
-  await page.goto(url);
+  await page.goto(iterUrl);
 
   let first = false;
   page.on('console', async msg => {
@@ -28,8 +29,11 @@ async function run(outfile, config = {}) {
       const records = JSON.parse(msg.text());
       if (!first) {
         const headers = Object.keys(records[0]).join(',');
-        stream.write(headers + '\n', 'utf-8');
-        first = true;
+        // Only write headers on first pass.
+        if (iter === 0) {
+          stream.write(headers + '\n', 'utf-8');
+          first = true;
+        }
       }
       for (const record of records) {
         const values = Object.values(record).join(',');
@@ -42,15 +46,25 @@ async function run(outfile, config = {}) {
 function main() {
   const args = process.argv.slice(2);
   if (args.length < 1) {
-    console.log("./benchmark.js <filename> [--no-headless] [--ignore-https-errors] [chrome_flags]")
-    process.exit(1);
+    exitOnBadArgs('No args found.');
   }
   const outfile = args[0];
-  const flags = new Set(args.slice(1));
+  const flags = new Set(args.slice(1, -2));
   const headless = !flags.delete('--no-headless');
   const ignoreHTTPSErrors = flags.has("--ignore-https-errors");
+  const [iterArg] = args.slice(-2);
+  if (iterArg !== '-iter') {
+    exitOnBadArgs(`-iter arg is not correct - found ${iterArg}`);
+  }
+  const [iter] = args.slice(-1);
   console.log("running with args: ", { headless, ignoreHTTPSErrors, args: [...flags] });
-  run(outfile, { headless, ignoreHTTPSErrors, args: [...flags] });
+  run(outfile, { headless, ignoreHTTPSErrors, args: [...flags] }, Number(iter));
+}
+
+function exitOnBadArgs(message) {
+  console.log(message)
+  console.log("./benchmark-iter.js <filename> [--no-headless] [--ignore-https-errors] [chrome_flags] [-iter i]")
+  process.exit(1);
 }
 
 main();
---
+++ run_benchmark.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit
+die() { set +v; echo "$*" 1>&2 ; exit 1; }
+
+# Run 10 iterations.
+for i in $(seq 0 10); do 
+   node ./benchmark-iter.js $@ -iter $i
+done
